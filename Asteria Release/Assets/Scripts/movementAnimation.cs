463aea2d635401ee0a8b477149e8fbaeb3d86243
malithaHarshamal <harshamalmalitha@gmail.com>
2024-01-11 02:03:54 +0530

    run

--- Asteria Release/Assets/Scripts/movementAnimation.cs
+++ Asteria Release/Assets/Scripts/movementAnimation.cs
@@ -8,25 +8,38 @@ public class movementAnimation : MonoBehaviour
 {
     Animator animator;
     int IsWalkingHash;
+    int IsRunningHash;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
         IsWalkingHash = Animator.StringToHash("IsMoving");
+        IsRunningHash = Animator.StringToHash("IsRunning");
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool IsRunning = animator.GetBool(IsRunningHash);
         bool IsWalking = animator.GetBool(IsWalkingHash);
         bool forwardPressed = Input.GetKey("w");
-        if (Input.GetKey("w"))
+        bool runPressed = Input.GetKey("left shift");
+
+        if (!IsWalking && forwardPressed)
         {
-            animator.SetBool("IsMoving", true);
+            animator.SetBool(IsWalkingHash, true);
         }
         if (IsWalking && !forwardPressed)
         {
             animator.SetBool(IsWalkingHash, false);
+        }
+        if (!IsRunning && (forwardPressed && runPressed))
+        {
+            animator.SetBool(IsRunningHash, true);
+        }
+        if (IsRunning && (!forwardPressed || !runPressed))
+        {
+            animator.SetBool(IsRunningHash, false);
         }
     }
 }
